a6e1d01269df3b3d239b006579c34e7976691b42
ShintaNakama <shintanakama77@gmail.com>
2019-11-24 18:50:26 +0900

    パラメータ取得のtest

--- gcf/cloud_function.go
+++ gcf/cloud_function.go
@@ -3,20 +3,21 @@ package gcf
 import (
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 func Search(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	// POSTの場合、Query()は使えない
 	v := r.URL.Query()
 	if v == nil {
 		return
 	}
+	// fmt.Fprintf(w, "%t\n", v)
 	host := "http://localhost:8983/solr/"
 	c := "group"
 	a := "select"
-	q := []string{"*:*"}
-	fq := []string{}
+	q := "*:*"
+	var fq string
 	st := "0"
 	rows := "1"
 	wt := "json"
@@ -25,42 +26,15 @@ func Search(w http.ResponseWriter, r *http.Request) {
 	var fl string
 	// var recommend string
 
-	for key, vs := range v {
-		fmt.Fprintf(w, "%s = %s\n", key, vs)
-		switch {
-		case strings.HasPrefix(key, "core"):
-			c = vs[0]
-		case strings.HasPrefix(key, "q"):
-			vv := key + ":"
-			q = append(q, vv)
-		case strings.HasPrefix(key, "fq"):
-		case strings.HasPrefix(key, "action"):
-			a = vs[0]
-		case strings.HasPrefix(key, "start"):
-			st = vs[0]
-		case strings.HasPrefix(key, "rows"):
-			rows = vs[0]
-		case strings.HasPrefix(key, "sort"):
-			sort = vs[0]
-		case strings.HasPrefix(key, "fl"):
+	fmt.Fprintf(w, "%s\n", r.FormValue("core"))
+	fmt.Fprintf(w, "%s\n", r.FormValue("q"))
+	// q = r.FormValue("q")
 
-		}
-	}
-	// b, err := ioutil.ReadAll(r.Body)
-	// fmt.Println(b)
-	// if err != nil {
-	// 	log.Printf("ReadAllError: %v\n", err)
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	w.Write([]byte(err.Error()))
-	// 	return
-	// }
-	// parsed, err := url.ParseQuery(string(b))
-	// if err != nil {
-	// 	log.Printf("ParceQueryError: %v\n", err)
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	w.Write([]byte(err.Error()))
-	// 	return
-	// }
-	// fmt.Println(toJSON(parsed))
+	fmt.Fprintf(w, "%s\n", v["q"])
+	// q = v["q"]
+	// fmt.Fprintf(w, "%t\n", v["q"][0])
+	// fmt.Fprintf(w, "%s\n", v["q"][1])
+
+	fmt.Println(host, c, a, q, fq, st, rows, wt, indent, sort, fl)
 
 }
